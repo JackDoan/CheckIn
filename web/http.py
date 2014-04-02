b66e22e94aed69eac54b1f1dc43874e1c993c4dc
Jack Doan <me@jackdoan.com>
2014-04-02 11:18:40 -0500

    Make dates human-friendly & sort in descending order

--- web/http.py
+++ web/http.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 from flask import Flask
 from flask import render_template
-import sqlite3
+import sqlite3, time
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-	conn = sqlite3.connect("../datt.db")
+	conn = sqlite3.connect("../chn.db")
         db = conn.cursor()
-        db.execute("Select rowid,* from records")
+        db.execute("Select rowid,* from records order by rowid desc")
         t_records = db.fetchall()
 	records = map(list, t_records)
 	i = 0
 	for r in records:
 	  db.execute("Select name from students where rowid=?", (r[1],))
 	  r[1] = str(db.fetchone()[0])
+	  r[3] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(r[3]))
         conn.close()
         return render_template('data.html', records=records)
 @app.route('/test')
