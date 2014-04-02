8e95c09ab8fe456fe6230f425fece1023e24e366
Jack Doan <me@jackdoan.com>
2014-04-02 14:09:22 -0500

    Changed the font from Helevetica to Droid Sans.

--- web/http.py
+++ web/http.py
@@ -3,8 +3,40 @@ from flask import Flask
 from flask import render_template
 import sqlite3, time
 
+from functools import wraps
+from flask import request, Response
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 app = Flask(__name__)
+
 @app.route('/')
+def login():
+	return render_template('login.html')
+
+@app.route('/data')
+@requires_auth
 def hello_world():
 	conn = sqlite3.connect("../chn.db")
         db = conn.cursor()
